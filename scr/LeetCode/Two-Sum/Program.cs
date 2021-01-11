cf7534f4e2b29d5bd298d8053933234576e7de01
Raj Hiray <raj.hiray@outlook.com>
2021-01-11 19:32:39 +0530

    Two Sum Problem.

---
+++ scr/LeetCode/Two-Sum/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Two_Sum
+{
+    class Program
+    {
+        /* 1. Two Sum          
+        Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+        You may assume that each input would have exactly one solution, and you may not use the same element twice.
+        You can return the answer in any order.
+
+        Example 1:
+            Input: nums = [2,7,11,15], target = 9
+            Output: [0,1]
+            Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+        Example 2:
+            Input: nums = [3,2,4], target = 6
+            Output: [1,2]
+
+        Example 3:
+            Input: nums = [3,3], target = 6
+            Output: [0,1]
+ 
+        Constraints:
+            2 <= nums.length <= 103
+            -109 <= nums[i] <= 109
+            -109 <= target <= 109
+            Only one valid answer exists.         
+         */
+
+        static void Main(string[] args)
+        {
+
+            Solution solution = new();
+            int[] result;
+
+            var input1 = new int[] { 2, 7, 11, 15 };
+            result = solution.TwoSum(input1,9);
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+            var input2 = new int[] { 3, 2, 4 };
+            result = solution.TwoSum(input2,6);           
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+
+            }
+        }
+    }
+}
---
+++ scr/LeetCode/Two-Sum/Solution.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Two_Sum
+{
+    public class Solution
+    {
+        /// <summary>
+        /// return indices of the two numbers such that they add up to target.
+        /// Brute Force
+        /// </summary>
+        /// <param name="nums">Given an array of integers nums and an integer</param>
+        /// <param name="target">target</param>
+        /// <returns>result</returns>
+        public int[] TwoSum1(int[] nums, int target)
+        {
+            int[] result = null;
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (target == nums[i] + nums[j])
+                        result = new int[] { i, j };
+                }
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// return indices of the two numbers such that they add up to target.
+        /// </summary>
+        /// <param name="nums">Given an array of integers nums and an integer</param>
+        /// <param name="target">target</param>
+        /// <returns>result</returns>
+        public int[] TwoSum(int[] nums, int target)
+        {
+            int[] result = null;
+            List<int> elements = new List<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int remaining = target - nums[i];
+                if(elements.Contains(remaining))
+                {
+                    return new int[] {elements.IndexOf(remaining),i};
+                }
+                elements.Add(nums[i]);
+            }
+            return result;
+        }
+    }
+}
