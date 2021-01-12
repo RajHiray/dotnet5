b768914bc920b872097ae21fe49f5415de367430
Raj Hiray <raj.hiray@outlook.com>
2021-01-13 00:29:46 +0530

    Product_of_Array_Except_Self

---
+++ scr/LeetCode/Product-of-Array-Except-Self/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Product_of_Array_Except_Self
+{
+    class Program
+    {
+        /* 238. Product of Array Except Self
+         Given an array nums of n integers where n > 1,  
+        return an array output such that output[i] is equal to the product of all the elements of nums except nums[i].
+
+        Example:
+        Input:  [1,2,3,4]
+        Output: [24,12,8,6]
+        Constraint: It's guaranteed that the product of the elements of any prefix or suffix of the array (including the whole array) fits in a 32 bit integer.
+
+        Note: Please solve it without division and in O(n).
+
+        Follow up:
+        Could you solve it with constant space complexity? (The output array does not count as extra space for the purpose of space complexity analysis.)
+
+         */
+        static void Main(string[] args)
+        {
+
+            Solution solution = new();
+            int[] result;
+
+            var input1 = new int[] { 2, 7, 11, 15 };
+            result = solution.ProductExceptSelf(input1);
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+            var input2 = new int[] {1, 2, 3, 4};
+            result = solution.ProductExceptSelf(input2);
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+
+            }
+        }
+    }
+}
---
+++ scr/LeetCode/Product-of-Array-Except-Self/Solution.cs
@@ -0,0 +1,29 @@
+﻿namespace Product_of_Array_Except_Self
+{
+    public class Solution
+    {
+        public int[] ProductExceptSelf(int[] nums)
+        {
+            int lenght = nums.Length,product=1;
+
+            // calculate product left to right
+            int[] result = new int[lenght];
+            result[0] = 1;
+            for (int i = 1; i < lenght; i++)
+            {
+                result[i] = product * nums[i-1];
+                product *= nums[i-1];
+            }
+
+            // calculate product right to left and product with result.
+            product = 1;            
+            for (int i= lenght-2; i >=0 ; i--)
+            {
+                result[i] *= product * nums[i+1];
+                product *= nums[i+1];
+            }
+
+            return result;
+        }
+    }
+}
