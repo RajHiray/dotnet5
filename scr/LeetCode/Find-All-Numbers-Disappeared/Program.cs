f291237efd6503767a8641b6f7ff0170d2fc383b
Raj Hiray <raj.hiray@outlook.com>
2021-01-17 15:43:04 +0530

    FindDisappearedNumbers

---
+++ scr/LeetCode/Find-All-Numbers-Disappeared/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Find_All_Numbers_Disappeared
+{
+    /* 448. Find All Numbers Disappeared in an Array
+    Given an array of integers where 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
+    Find all the elements of [1, n] inclusive that do not appear in this array.
+
+    Could you do it without extra space and in O(n) runtime? You may assume the returned list does not count as extra space.
+    Example:
+        Input:
+        [4,3,2,7,8,2,3,1]
+        Output:
+        [5,6]     
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new();
+            IList<int> result;
+
+            var input1 = new int[] { 4, 3, 2, 7, 8, 2, 3, 1 };
+            result = solution.FindDisappearedNumbers(input1);
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+            var input2 = new int[] { 3, 2, 4,2,3 };
+            result = solution.FindDisappearedNumbers(input2);
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+
+            }
+        }
+    }
+}
---
+++ scr/LeetCode/Find-All-Numbers-Disappeared/Solution.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Find_All_Numbers_Disappeared
+{
+    public class Solution
+    {
+        /// <summary>
+        ///  Find all the elements of [1, n] inclusive that do not appear in this array.
+        /// </summary>
+        /// <param name="nums">Given an array of integers where 1 ≤ a[i] ≤ n (n = size of array)</param>
+        /// <returns></returns>
+        public IList<int> FindDisappearedNumbers(int[] nums)
+        {
+            IList<int> result = new List<int>();
+            int lenght = nums.Length, item;
+            for (int i = 0; i < lenght; i++)
+            {
+                item = Math.Abs(nums[i]) - 1;
+                nums[item] = nums[item] > 0 ? -nums[item] : nums[item];
+            }
+            for (int i = 0; i < lenght; i++)
+            {
+                if (nums[i] > 0)
+                    result.Add(i + 1);
+            }
+            return result;
+        }
+
+        /// <summary>
+        ///  Find all the elements of [1, n] inclusive that do not appear in this array.
+        ///  V0
+        /// </summary>
+        /// <param name="nums">Given an array of integers where 1 ≤ a[i] ≤ n (n = size of array)</param>
+        /// <returns></returns>
+        public IList<int> FindDisappearedNumbersV0(int[] nums)
+        {
+            return Enumerable.Range(1, nums.Length).ToList().Except(nums.ToList()).ToList();
+        }
+    }
+}
