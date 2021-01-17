90b4983f6cf862cdb5d7f5a493ddded16bd73290
Raj Hiray <raj.hiray@outlook.com>
2021-01-17 17:15:16 +0530

    FindDuplicates

---
+++ scr/LeetCode/Find-All-Duplicates-In-Array/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Find_All_Duplicates_In_Array
+{
+    class Program
+    {
+        /* 442. Find All Duplicates in an Array
+        Given an array of integers, 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
+        Find all the elements that appear twice in this array.
+        Could you do it without extra space and in O(n) runtime?
+
+        Example:
+            Input:
+            [4,3,2,7,8,2,3,1]
+            Output:
+            [2,3]
+         */
+        static void Main(string[] args)
+        {
+            Solution solution = new();
+            IList<int> result;
+
+            var input1 = new int[] { 4, 3, 2, 7, 8, 2, 3, 1 };
+            result = solution.FindDuplicates(input1);
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+            var input2 = new int[] { 4, 3, 4, 2, 3 };
+            result = solution.FindDuplicates(input2);
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+
+            }
+        }
+    }
+}
---
+++ scr/LeetCode/Find-All-Duplicates-In-Array/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Find_All_Duplicates_In_Array
+{
+    public class Solution
+    {
+        /// <summary>
+        /// Find all the elements that appear twice in this array.
+        /// </summary>
+        /// <param name="nums">Given an array of integers, 1 ≤ a[i] ≤ n (n = size of array)</param>
+        /// <returns></returns>
+        public IList<int> FindDuplicates(int[] nums)
+        {
+            IList<int> result = new List<int>();
+            int lenght = nums.Length, item;
+            for (int i = 0; i < lenght; i++)
+            {
+                item = Math.Abs(nums[i]) - 1;
+                if (nums[item] < 0)
+                    result.Add(item + 1);
+                nums[item] = -Math.Abs(nums[item]);
+            }
+            return result;
+        }
+    }
+}
