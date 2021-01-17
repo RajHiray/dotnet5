1846c25306b5d176cf346e06cf7b8694c4f64f54
Raj Hiray <raj.hiray@outlook.com>
2021-01-17 18:55:28 +0530

    SetZeroes

---
+++ scr/LeetCode/Set-Matrix-Zeroes/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Set_Matrix_Zeroes
+{
+    class Program
+    {
+        /* 73. Set Matrix Zeroes
+        Given an m x n matrix. If an element is 0, set its entire row and column to 0. Do it in-place.
+
+        Follow up:
+        A straight forward solution using O(mn) space is probably a bad idea.
+        A simple improvement uses O(m + n) space, but still not the best solution.
+        Could you devise a constant space solution?
+ 
+        Example 1:
+            Input: matrix = [[1,1,1],[1,0,1],[1,1,1]]
+            Output: [[1,0,1],[0,0,0],[1,0,1]]
+
+        Example 2:
+            Input: matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+            Output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]] 
+
+        Constraints:
+            m == matrix.length
+            n == matrix[0].length
+            1 <= m, n <= 200
+            -2^31 <= matrix[i][j] <= 2^31 - 1
+         */
+        static void Main(string[] args)
+        {
+            Solution solution = new();
+
+            var input1 = new int[][] {
+                new []{ 1, 1, 1 },
+                new []{ 1, 0, 1 },
+                new []{ 1, 1, 1 }
+            };
+
+            var input2 = new int[][] {
+                new []{ 0,1,2,0 },
+                new []{ 3,4,5,2 },
+                new []{ 1, 3, 1, 5 }
+            };
+
+            solution.SetZeroes(input1);
+            solution.SetZeroes(input2);
+        }
+    }
+}
---
+++ scr/LeetCode/Set-Matrix-Zeroes/Solution.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Set_Matrix_Zeroes
+{
+    public class Solution
+    {
+        /// <summary>
+        /// If an element is 0, set its entire row and column to 0
+        /// </summary>
+        /// <param name="matrix"></param>
+        public void SetZeroes(int[][] matrix)
+        {
+            int r = matrix.Length;
+            int c = matrix[0].Length;
+            bool root = false;
+            for (int i = 0; i < r; i++)
+            {
+                if (matrix[i][0] == 0)
+                {
+                    root = true;
+                }
+                for (int j = 1; j < c; j++)
+                {
+                    if (matrix[i][j] == 0)
+                    {
+                        matrix[i][0] = 0;
+                        matrix[0][j] = 0;
+                    }
+                }
+            }
+
+            for (int i = 1; i < r; i++)
+            {
+                for (int j = 1; j < c; j++)
+                {
+                    if (matrix[i][0] == 0 || matrix[0][j] == 0)
+                    {
+                        matrix[i][j] = 0;
+                    }
+                }
+            }
+
+            // See if the first row needs to be set to zero as well
+            if (matrix[0][0] == 0)
+            {
+                for (int j = 0; j < c; j++)
+                {
+                    matrix[0][j] = 0;
+                }
+            }
+
+            // See if the first column needs to be set to zero as well
+            if (root)
+            {
+                for (int i = 0; i < r; i++)
+                {
+                    matrix[i][0] = 0;
+                }
+            }
+        }
+    }
+}
