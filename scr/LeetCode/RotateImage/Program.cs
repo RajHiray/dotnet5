1efdb01c2ae8aa7b13874f9ec1e04cfcfe17b1b4
Raj Hiray <raj.hiray@outlook.com>
2021-03-10 00:03:09 +0530

    RotateImage

---
+++ scr/LeetCode/RotateImage/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace RotateImage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var input1 = new int[][]  {
+                new int[]{1,2,3,4},
+                new int[]{5,6,7,8},
+                new int[]{9,10,11,12},
+                new int[]{13,14,15,16}
+                };
+            Solution solution = new Solution();
+            solution.Rotate(input1);        
+        }
+    }
+}
---
+++ scr/LeetCode/RotateImage/Solution.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RotateImage
+{
+    class Solution
+    {
+        public void Rotate(int[][] matrix)
+        {
+            int n = matrix.Length, temp;
+            int l = 0, r = n - 1, i, top, btm;
+
+            while (l < r)
+            {
+                top = l;
+                btm = r;
+                for (i = 0; i < r - l; i++)
+                {
+                    temp = matrix[top][l + i];
+                    matrix[top][l + i] = matrix[btm - i][l];
+                    matrix[btm - i][l] = matrix[btm][r - i];
+                    matrix[btm][r - i] = matrix[top + i][r];
+                    matrix[top + i][r] = temp;
+                }
+                r--;
+                l++;
+            }
+        }
+    }
+}
