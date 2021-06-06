6b5773128abf0f8dfdab1a3fbb8aca988ded5adc
Raj Hiray <raj.hiray@outlook.com>
2021-06-06 19:40:22 +0530

    42. Trapping Rain Water

---
+++ scr/LeetCode/TrappingRain/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TrappingRain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new();
+            int result;
+
+            var input1 = new int[] { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
+            result = solution.Trap(input1);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ scr/LeetCode/TrappingRain/Solution.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrappingRain
+{
+    class Solution
+    {
+        public int Trap(int[] height)
+        {
+            if (height == null || height.Length <= 0)
+            {
+                return 0;
+            }
+            int leftMax = 0, pl = 0, pr = height.Length - 1, totalArea = 0, rightMax = 0;
+
+            while (pl < pr)
+            {
+                if (height[pl] <= height[pr])
+                {
+                    rightMax = Math.Max(rightMax, height[pr]);
+                    if (leftMax <= height[pl])
+                    {
+                        leftMax = height[pl];
+                    }
+                    else
+                    {
+                        totalArea += Math.Min(rightMax, leftMax) - height[pl];
+                    }
+                    pl++;
+                }
+                else
+                {
+                    leftMax = Math.Max(leftMax, height[pl]);
+                    if (rightMax <= height[pr])
+                    {
+                        //rightMax= Math.Max(leftMax,height[pr]);
+                        rightMax = height[pr];
+                    }
+                    else
+                    {
+                        totalArea += Math.Min(rightMax, leftMax) - height[pr];
+                    }
+                    pr--;
+                }
+            }
+            return totalArea;
+        }
+    }
+}
